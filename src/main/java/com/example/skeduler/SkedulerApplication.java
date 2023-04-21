ba18f71ac784edae864debdad60603364df3663f
sangyoon kim <sangyun0904@gmail.com>
2023-04-21 14:06:30 +0900

    Create project skeduler with spring web dependency

---
+++ src/main/java/com/example/skeduler/SkedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.skeduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skeduler/SkedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skeduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
