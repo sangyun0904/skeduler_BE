10f62b7dd76e553fb1fd621d06f278d0a7681347
sangyoon kim <sangyun0904@gmail.com>
2023-05-01 10:55:44 +0900

    Fix controller and add thymeleaf

--- src/main/java/com/example/skeduler/HomeController.java
+++ src/main/java/com/example/skeduler/HomeController.java
@@ -1,14 +1,14 @@
-//package com.example.skeduler;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class HomeController {
-//
-//    @GetMapping("/")
-//    public String index() {
-//        return "Greetings from Spring Boot!";
-//    }
-//
-//}
+package com.example.skeduler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
