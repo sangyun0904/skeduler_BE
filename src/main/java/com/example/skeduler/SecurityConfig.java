733bcf05a0a9f0700a82d3ff9a0e24a25b45af96
sangyoon kim <sangyun0904@gmail.com>
2023-05-02 12:24:18 +0900

    Create github login and logout

---
+++ src/main/java/com/example/skeduler/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.skeduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdopter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .oauth2Login();
+        // @formatter:on
+    }
+
+}
