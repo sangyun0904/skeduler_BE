f845a3277caf0853f972be190f132c5fd9e6c726
sangyoon kim <sangyun0904@gmail.com>
2023-05-03 17:44:05 +0900

    Fix security config

--- src/main/java/com/example/skeduler/SecurityConfig.java
+++ src/main/java/com/example/skeduler/SecurityConfig.java
@@ -1,26 +1,28 @@
 package com.example.skeduler;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdopter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests(a -> a
-                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .requestMatchers("/", "/error", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
             )
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             )
             .oauth2Login();
-        // @formatter:on
+
+        return http.build();
     }
 
 }
