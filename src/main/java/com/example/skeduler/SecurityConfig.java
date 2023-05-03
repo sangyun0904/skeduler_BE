3aa5509bef17537dbc8b0b619eae6c22279e3235
sangyoon kim <sangyun0904@gmail.com>
2023-05-03 18:25:36 +0900

    Add spring security config logout

--- src/main/java/com/example/skeduler/SecurityConfig.java
+++ src/main/java/com/example/skeduler/SecurityConfig.java
@@ -4,15 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf()
+                .disable()
             .authorizeRequests(a -> a
                 .requestMatchers("/", "/error", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
@@ -20,7 +23,10 @@ public class SecurityConfig {
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             )
-            .oauth2Login();
+            .oauth2Login()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
 
         return http.build();
     }
