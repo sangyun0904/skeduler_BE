d2c46399c82a926312611012039e3ae8d27d81bd
sangyoon kim <sangyun0904@gmail.com>
2023-05-14 09:47:57 +0900

    Add member, task Data annot

--- src/main/java/com/example/skeduler/model/Task.java
+++ src/main/java/com/example/skeduler/model/Task.java
@@ -4,13 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Task {
 
